8d53099906a225bc1c50be4e438017bbb3db764f
abc <will.h.wilson@gmail.com>
2022-02-27 19:33:27 -0500

    add logic step js

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -61,13 +61,34 @@ function styleInfo(feature) {
   return {
     opacity: 1,
     fillOpacity: 1,
-    fillColor: "#ffae42",
+    fillColor: getColor(feature.properties.mag),
     color: "#000000",
-    radius: getRadius(),
+    radius: getRadius(feature.properties.mag),
     stroke: true,
     weight: 0.5
   };
 }
+
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+}
+
 function getRadius(magnitude) {
   if (magnitude === 0) {
     return 1;
@@ -81,6 +102,10 @@ function getRadius(magnitude) {
       console.log(data);
       return L.circleMarker(latlng);
     },
-    style: styleInfo
+    style: styleInfo,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+
   }).addTo(map);
 });
